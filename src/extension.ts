bf2729eef29f56d3efb354fbc508a707b174cdd3
BaileyFirman <bailey.firman@nintex.com>
2020-11-14 10:41:14 +1300

    Initial sorting of module only lines

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,66 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-// import * as fs from 'fs';
+import * as fs from 'fs';
+// Entire Module
+import * as validator from "./ZipCodeValidator";
+// Default Import
+import $ from "jquery";
+// Default + Module
+import Default, { SubModule } from "jquery";
+// Single Module
+import { ZipCodeValidator } from "./ZipCodeValidator";
+// Renamed Module
+import { ZipCodeValidator as ZCV, } from "./ZipCodeValidator";
+// Type Import
+import type { APIResponseType } from "./api";
+// Side Effect Module
+import "./my-module.js";
 
-const sortLines = (text: string) => text.split('\n').sort().join('\n');
+
+const trimImport = (module: string) => {
+    return module.slice(7);
+}
+
+const getModules = (lines: string[], allDependencies: string[]) => {
+    return lines.filter(line => {
+        const subline: string[] = line.split(' ');
+        const module: string = subline[subline.length - 1].split('\"')[1];
+        return allDependencies.includes(module);
+    })
+};
 
 const sortLinesExtension = () => {
-	let editor = vscode.window.activeTextEditor;
+    let editor = vscode.window.activeTextEditor;
+    let root = vscode.workspace.rootPath ?? "";
+    const packageJsonPath = path.join(root, 'package.json');
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+    const dependencies: string[] = Object.keys(packageJson.dependencies);
+    const devDependencies: string[] = Object.keys(packageJson.devDependencies);
+    const allDependencies = [...dependencies, ...devDependencies];
+    console.log(allDependencies);
 
 	if (editor) {
 		let document = editor.document;
 		let selection = editor.selection;
 
-		let selectionText = document.getText(selection);
-		let sortedSelection = sortLines(selectionText);
+        const module = [];
+        const defaults = [];
+        const defaultAndModules = [];
+        const singleModule = [];
+        const typeModule = [];
+        const sideEffect = []
+
+
+	    const selectionText: string = document.getText(selection);
+        const selectionLines: string[] = selectionText.split('\n');
+        // const dependancyLines: string[] = getModules(selectionLines, allDependencies);
+        // const resultLines: string = dependancyLines.join('\n');
 		
-		editor.edit(editBuilder => editBuilder.replace(selection, sortedSelection));
+		editor.edit(editBuilder => editBuilder.replace(selection, resultLines));
 	}
 };
 
 export const activate = (context: vscode.ExtensionContext) => {
     let disposable = vscode.commands.registerCommand('extension.sortTypeScriptImports', sortLinesExtension);
-    let root = vscode.workspace.rootPath ?? "";
-    const packageJsonPath = path.join(root, 'package.json');
 	context.subscriptions.push(disposable);
 }
